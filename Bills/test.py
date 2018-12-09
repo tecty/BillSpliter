f16556935ce6e45ceba21468e94f0764b1e47be1
Toby HUANG <619058900@qq.com>
2018-12-09 17:53:26 +1100

    resume flow is corrected

--- Bills/test.py
+++ Bills/test.py
@@ -110,3 +110,90 @@ class BillCase(TestCase):
                 from_u=self.user002
             ).state
         )
+
+    def test_approve_multiple_user(self):
+        bill = Bill.objects.create(
+            title="dinner",
+            description="nothing",
+            owner=self.user001,
+            group=self.group
+        )
+        tr1 = bill.transaction_set.create(
+            from_u=self.user002,
+            to_u=self.user001,
+            amount=1.0
+        )
+        tr2 = bill.transaction_set.create(
+            from_u=self.user001,
+            to_u=self.user001,
+            amount=1.0
+        )
+        bill.approve(self.user001)
+        # this state must be prepare
+        self.assertEqual(PREPARE, bill.state)
+        bill.approve(self.user002)
+        self.assertEqual(CONCENCUS, bill.state)
+
+        # all should be suspend
+        self.assertEqual(
+            CONCENCUS,
+            bill.transaction_set.get(
+                from_u=self.user001
+            ).state
+        )
+        self.assertEqual(
+            CONCENCUS,
+            bill.transaction_set.get(
+                from_u=self.user002
+            ).state
+        )
+
+    def test_resume_other_user(self):
+        bill = Bill.objects.create(
+            title="dinner",
+            description="nothing",
+            owner=self.user001,
+            group=self.group
+        )
+        tr1 = bill.transaction_set.create(
+            from_u=self.user002,
+            to_u=self.user001,
+            amount=1.0
+        )
+        tr2 = bill.transaction_set.create(
+            from_u=self.user001,
+            to_u=self.user001,
+            amount=1.0
+        )
+        bill.approve(self.user001)
+        bill.reject(self.user002)
+
+        # only owner can try to resume it
+        bill.resume(user.u002)
+        self.assertEqual(SUSPEND, bill.state)
+
+    def test_resume_other_user(self):
+        bill = Bill.objects.create(
+            title="dinner",
+            description="nothing",
+            owner=self.user001,
+            group=self.group
+        )
+        tr1 = bill.transaction_set.create(
+            from_u=self.user002,
+            to_u=self.user001,
+            amount=1.0
+        )
+        tr2 = bill.transaction_set.create(
+            from_u=self.user001,
+            to_u=self.user001,
+            amount=1.0
+        )
+        bill.approve(self.user001)
+        bill.reject(self.user002)
+
+        # only owner can try to resume it
+        bill.resume(bill.owner)
+        self.assertEqual(PREPARE, bill.state)
+        bill.approve(self.user002)
+        self.assertEqual(CONCENCUS, bill.state)
