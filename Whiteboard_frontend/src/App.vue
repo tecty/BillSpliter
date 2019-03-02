39363144d1f9000b8c372034e2e1c50aaa7e18ec
Toby HUANG <619058900@qq.com>
2019-03-03 03:33:08 +1100

    style enhancement

--- Whiteboard_frontend/src/App.vue
+++ Whiteboard_frontend/src/App.vue
@@ -1,13 +1,6 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer
-      fixed
-      abolute
-      clipped
-      class="grey lighten-4"
-      app
-      v-model="drawer"
-    >
+    <v-navigation-drawer fixed abolute clipped class="grey lighten-4" app v-model="drawer">
       <v-list dense class="grey lighten-4">
         <template v-for="(item, i) in items">
           <v-layout row v-if="item.heading" align-center :key="i">
@@ -20,47 +13,37 @@
                 flat
                 :to="item.href ? { name: item.href } : null"
                 class="grey--text"
-                >{{ item.text }}</v-btn
-              >
+              >{{ item.text }}</v-btn>
             </v-flex>
           </v-layout>
-          <v-divider
-            dark
-            v-else-if="item.divider"
-            class="my-3"
-            :key="i"
-          ></v-divider>
-          <v-list-tile
-            :key="i"
-            v-else
-            :to="item.href ? { name: item.href } : null"
-          >
+          <v-divider dark v-else-if="item.divider" class="my-3" :key="i"></v-divider>
+          <v-list-tile :key="i" v-else :to="item.href ? { name: item.href } : null">
             <v-list-tile-action>
               <v-icon>{{ item.icon }}</v-icon>
             </v-list-tile-action>
             <v-list-tile-content>
-              <v-list-tile-title class="grey--text">{{
+              <v-list-tile-title class="grey--text">
+                {{
                 item.text
-              }}</v-list-tile-title>
+                }}
+              </v-list-tile-title>
             </v-list-tile-content>
           </v-list-tile>
         </template>
       </v-list>
     </v-navigation-drawer>
     <v-toolbar color="light-blue darken-2" app clipped-left tabs>
-      <v-toolbar-side-icon
-        @click.native="drawer = !drawer"
-      ></v-toolbar-side-icon>
+      <v-toolbar-side-icon @click.native="drawer = !drawer"></v-toolbar-side-icon>
       <v-toolbar-title to="{name:'home'}">Whiteboard</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items>
         <v-btn v-if="!username" to="login" flat>Login</v-btn>
-        <userAction v-else />
+        <userAction v-else/>
         <v-btn v-if="username" @click="logout" flat>Logout</v-btn>
       </v-toolbar-items>
     </v-toolbar>
     <v-content>
-      <router-view />
+      <router-view/>
     </v-content>
   </v-app>
 </template>
@@ -79,13 +62,13 @@ export default {
       items: [
         // { icon: "touch_app", text: "Reminders", href: "about" },
         { heading: "Billing System", text: "create", href: "billCreate" },
-        { icon: "add", text: "Transactions", href: "transaction" },
-        { icon: "add", text: "Bills", href: "bill" },
-        { icon: "add", text: "Settlement" },
+        { icon: "call_made", text: "Transactions", href: "transaction" },
+        { icon: "merge_type", text: "Bills", href: "bill" },
+        { icon: "shuffle", text: "Settlement" },
         { divider: true },
         { icon: "settings", text: "Settings", href: "about" },
-        { icon: "chat_bubble", text: "User" },
-        { icon: "chat_bubble", text: "Group", href: "group" },
+        { icon: "person", text: "User" },
+        { icon: "group", text: "Group", href: "group" },
         { icon: "help", text: "Help" },
         { icon: "phonelink", text: "App downloads" },
         { icon: "keyboard", text: "Keyboard shortcuts" }
--- Whiteboard_frontend/src/views/bill/Create.vue
+++ Whiteboard_frontend/src/views/bill/Create.vue
@@ -3,30 +3,26 @@
     <h1>Create Bill</h1>
     <form @submit.prevent="submit">
       <v-layout row wrap>
-        <v-flex xs12 md6 pa-1>
+        <v-flex xs12 md7 lg7 pa-1>
           <v-text-field label="Title" v-model="title"></v-text-field>
-        </v-flex>
-        <v-flex xs12 md6 lg3 pa-1>
-          <GroupSelector v-model="group" />
         </v-flex>
       </v-layout>
       <v-layout row wrap>
         <v-flex grow xs12 pa-1>
-          <v-text-field label="Description" v-model="description" />
+          <v-text-field label="Description" v-model="description"/>
         </v-flex>
       </v-layout>
       <!-- select phople in the group -->
-      <MemberSelector v-if="group" :group="group" v-model="userSelected" />
       <v-layout row wrap>
         <v-flex grow xs12 md6 lg4 pa-1>
-          <v-text-field
-            label="Total"
-            v-model="total"
-            prefix="$"
-            placeholder="xx.xx"
-          />
+          <v-text-field label="Total" v-model="total" prefix="$" placeholder="xx.xx"/>
+        </v-flex>
+        <v-flex xs12 md6 lg3 pa-1>
+          <GroupSelector v-model="group"/>
         </v-flex>
       </v-layout>
+
+      <MemberSelector v-if="group" :group="group" v-model="userSelected"/>
       <v-layout row wrap>
         <v-btn color="success" type="submit">Split it!</v-btn>
       </v-layout>
