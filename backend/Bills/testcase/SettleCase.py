90c78e0c334219a95f2ab1febcbed39659aa805f
Toby HUANG <z5141448@ad.unsw.edu.au>
2019-06-01 18:16:23 +0800

    remove duplicate code

--- backend/Bills/testcase/SettleCase.py
+++ backend/Bills/testcase/SettleCase.py
@@ -1,38 +1,8 @@
 from .util import *
 
-class SettleCase(TestCase):
-    def setUp(self):
-        """Create a list of User to be test environment"""
-        self.user001 = User.objects.create_user(
-            'u001', "u001@example.cn", "tt")
-        self.user002 = User.objects.create_user(
-            'u002', "u002@example.cn", "tt")
-        self.user003 = User.objects.create_user(
-            'u003', "u003@example.cn", "tt")
-        self.user004 = User.objects.create_user(
-            'u004', "u004@example.cn", "tt")
-
-        # dummy billing groups
-        self.group = BillGroups.objects.create(name="test", owner=self.user001)
-        self.group.addUser(self.user001.id)
-        self.group.addUser(self.user002.id)
-        self.group.addUser(self.user003.id)
-        self.group.addUser(self.user004.id)
-        self.user001.groups.add(self.group)
-
-        # ul for user_list
-        self.ul = [
-            self.user001,
-            self.user002,
-            self.user003,
-            self.user004,
-        ]
-
-        # creating a lot of finished tr
-
-        create_bill(self.ul, 10)
-        create_bill(self.ul, 20)
-        create_bill(self.ul, 30)
+class SettleCase(UserCaseEnv):
+    # def setUp(self):
+    #     super().setUp()
 
     def create_settlement(self):
         return Settlement.objects.create(
